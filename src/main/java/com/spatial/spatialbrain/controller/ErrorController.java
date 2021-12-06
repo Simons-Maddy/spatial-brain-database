1dfd92b833c7d37310f4cc3004bbc15bbf883823
Chunfu Shaw <xiaochunfu528@163.com>
2021-12-06 11:19:06 +0800

    all files of database

---
+++ src/main/java/com/spatial/spatialbrain/controller/ErrorController.java
@@ -0,0 +1,21 @@
+package com.spatial.spatialbrain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+    @RequestMapping("/404")
+    public String to404() {
+        return "error/404";
+    }
+
+    @RequestMapping("/500")
+    public String to500() {
+        return "error/500";
+    }
+}
+
+
---
+++ src/main/java/com/spatial/spatialbrain/mapper/SlideseqLocationMapper.java
@@ -0,0 +1,10 @@
+/**
+ * @author oyc
+ * @Description:
+ * @date 2018/7/8 22:51
+ */
+
+package com.spatial.spatialbrain.mapper;
+
+public interface SlideseqLocationMapper {
+    }
