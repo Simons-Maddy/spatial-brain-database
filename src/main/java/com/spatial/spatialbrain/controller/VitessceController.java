8d03548ffb2b712bda172bf1e9eeed97638bc961
Chunfu Shaw <xiaochunfu528@163.com>
2021-12-09 21:12:18 +0800

    all files of database

--- src/main/java/com/spatial/spatialbrain/controller/VitessceController.java
+++ src/main/java/com/spatial/spatialbrain/controller/VitessceController.java
@@ -1,13 +1,14 @@
 package com.spatial.spatialbrain.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-    @RequestMapping("/hello")
+@Controller
+public class VitessceController {
+    @RequestMapping("/vitessce")
     public String hello() {
-        return "Hello Spring Boot!";
+        return "vitessce";
     }
 }
 
